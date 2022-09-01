713d72c5d0bf3a1cb0e0811b6605f47e41e29da1
VOkhotnikova <valka382@gmail.com>
2022-09-01 19:32:23 +0200

    testing

---
+++ chapters/ch1/challenge2.py
@@ -0,0 +1,5 @@
+season = input('Please choose a season: ')
+number = input('Please choose a number: ')
+adjective = input('Please choose an adjective: ')
+
+print(f'On a {adjective} {season} day, I drink at least {number} cups of juice')
